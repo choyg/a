05d906a8a24105e92d808cd36fd9327271e4055c
Gensen Choy <gensen.choy@gmail.com>
2018-10-12 20:56:33 -0700

    More sensible APIs

--- public/graph.js
+++ public/graph.js
@@ -1,6 +1,5 @@
-function createGraph() {
+function graph(canvas) {
   const vertexCount = 100;
-  const canvas = document.getElementById('canvas');
   const renderer = canvas.getContext('2d');
   canvas.height = window.innerHeight - 50;
   canvas.width = window.innerWidth - 50;
@@ -17,65 +16,65 @@ function createGraph() {
   renderer.fillRect(start.x - 5, start.y - 5, 10, 10);
   renderer.fillRect(end.x - 5, end.y - 5, 10, 10);
   return { graph, startKey, endKey };
-}
 
-function renderGraph(renderer, vertices) {
-  renderer.fillStyle = 'black';
-  renderer.strokeStyle = 'grey';
-  vertices.forEach(vertex => {
-    vertex.vertices.forEach(key => {
-      const v = vertices.get(key);
-      renderer.beginPath();
-      renderer.moveTo(vertex.x, vertex.y);
-      renderer.lineTo(v.x, v.y);
-      renderer.stroke();
+  function renderGraph(renderer, vertices) {
+    renderer.fillStyle = 'black';
+    renderer.strokeStyle = 'grey';
+    vertices.forEach(vertex => {
+      vertex.vertices.forEach(key => {
+        const v = vertices.get(key);
+        renderer.beginPath();
+        renderer.moveTo(vertex.x, vertex.y);
+        renderer.lineTo(v.x, v.y);
+        renderer.stroke();
+      });
     });
-  });
-  vertices.forEach(v => renderer.fillRect(v.x - 5, v.y - 5, 10, 10));
-}
+    vertices.forEach(v => renderer.fillRect(v.x - 5, v.y - 5, 10, 10));
+  }
 
-/**
- * @param {number} count Number of vertices to generate
- * @param {number} width Width of canvas
- * @param {number} height Height of canvas
- * @returns {Map} vertices Map of Vertex objects
- */
-function generateVertices(count, width, height) {
-  const vertices = new Map();
-  for (let i = 0; i < count; i++) {
-    vertices.set(i, {
-      x: Math.random() * width,
-      y: Math.random() * height,
-      vertices: []
+  /**
+   * @param {number} count Number of vertices to generate
+   * @param {number} width Width of canvas
+   * @param {number} height Height of canvas
+   * @returns {Map} vertices Map of Vertex objects
+   */
+  function generateVertices(count, width, height) {
+    const vertices = new Map();
+    for (let i = 0; i < count; i++) {
+      vertices.set(i, {
+        x: Math.random() * width,
+        y: Math.random() * height,
+        vertices: []
+      });
+    }
+    return vertices;
+  }
+
+  /**
+   * Generates edges for given array of Vertex objects. Mutates an existing map
+   * @param {Map} vertices Map of Vertex objects
+   */
+  function generateEdges(vertices) {
+    vertices.forEach((v1, v1Key) => {
+      vertices.forEach((v2, v2Key) => {
+        // Prob of a vertex depends on dist
+        if (dist(v1, v2) < 200 && Math.random() < 0.4) {
+          v2.vertices.push(v1Key);
+          v1.vertices.push(v2Key);
+        }
+      });
     });
   }
-  return vertices;
-}
 
-/**
- * Generates edges for given array of Vertex objects. Mutates an existing map
- * @param {Map} vertices Map of Vertex objects
- */
-function generateEdges(vertices) {
-  vertices.forEach((v1, v1Key) => {
-    vertices.forEach((v2, v2Key) => {
-      // Prob of a vertex depends on dist
-      if (dist(v1, v2) < 200 && Math.random() < 0.4) {
-        v2.vertices.push(v1Key);
-        v1.vertices.push(v2Key);
-      }
-    });
-  });
-}
-
-/**
- * Distance Formula
- * @param {Vertex} a
- * @param {Vertex} b
- * @returns {number} Distance between points a and b
- */
-function dist(a, b) {
-  return Math.sqrt(Math.pow(b.y - a.y, 2) + Math.pow(b.x - a.x, 2));
+  /**
+   * Distance Formula
+   * @param {Vertex} a
+   * @param {Vertex} b
+   * @returns {number} Distance between points a and b
+   */
+  function dist(a, b) {
+    return Math.sqrt(Math.pow(b.y - a.y, 2) + Math.pow(b.x - a.x, 2));
+  }
 }
 
 /**
